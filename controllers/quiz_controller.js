6ff37ebaa55758e219f2861b5e2c8ac711c6d65e
migue091 <mdduran@gmail.com>
2015-07-30 17:38:46 +0200

    Arreglados problemas en lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,13 +3,20 @@ var models = require('../models/models.js');
 // GET /quizes/:id
 exports.index = function(req,res) {
 	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs',{quiz: quizes});
+		res.render('quizes/index.ejs',{quizes: quizes});
 	})
 };
 
-// GET /quizes/answer
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+   res.render('quizes/show', { quiz: quiz});
+  })
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req,res) {
-	models.Quiz.findAll(req.params.quizId).then(function(quiz) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
 		if (req.query.respuesta === quiz.respuesta) {
 			res.render('quizes/answer',{quiz: quiz, respuesta: 'Correcto'});
 		}
